129cc9448297fbef649d6fffbf7c7309eb0d0cea
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2022-06-09 15:47:01 +0200

    update

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-wrap">
-    <div class="w-full lg:w-8/12 bg-gray-800 py-6 px-6 rounded-3xl">
+    <div class="w-full lg:w-9/12 bg-gray-800 py-6 px-6 rounded-3xl">
       <!--div class="flex justify-between text-white items-center mb-8">
         <p class="text-2xl font-bold">{{ selectImage.name }}</p>
       </!div-->
       <div class="flex flex-wrap">
-        <el-image style="width: 100%; height: 100%" :src="selectImage.url" :preview-src-list="previewlist"
-          :initial-index="4" fit="cover" />
+        <el-image style="width: 100%; height: 71%" :src="selectImage.url" :preview-src-list="previewlist"
+          :initial-index="4" fit="contain" />
       </div>
     </div>
-    <div class="w-full mt-8 lg:mt-0 lg:w-4/12 lg:pl-4">
+    <div class="w-full mt-8 lg:mt-0 lg:w-3/12 lg:pl-4">
       <div class="bg-gray-800 rounded-3xl px-6 pt-6">
         <div class="flex text-white text-2xl pb-6 font-bold">
           <a href="#" class="block relative">
@@ -23,7 +23,7 @@
         <el-table-column type="selection" width="45" />
         <el-table-column align="center">
           <template #default="{ row }">
-            <el-image :src="row.url" fit="fill" @click="showImage(row)" />
+            <el-image :src="row.url" fit="contain" style="height: 210px; width:300px;" @click="showImage(row)" />
           </template>
         </el-table-column>
       </el-table>
